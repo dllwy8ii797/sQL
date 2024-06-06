bf2c2cb9905361277d50d328d721c752184d08c6
dllwy8ii797 <112999490+dllwy8ii797@users.noreply.github.com>
2024-06-06 15:41:09 +0800

    Update C07_09.sql

--- chapter 7/C07_09.sql
+++ chapter 7/C07_09.sql
@@ -1,1 +1,7 @@
-
+SELECT SS.store_id, SS.store_name, SS.shohin_id, S.shohin_name, S.sell_price, ZS.stock_amount
+ FROM StoreShohin AS SS INNER JOIN Shohin AS S
+ ON SS.shohin_id = S.shohin_id
+ INNER JOIN StockShohin AS ZS
+ ON SS.shohin_id = ZS.shohin_id
+ WHERE ZS.whouse_id= 'S001'
+ ORDER BY store_id;
