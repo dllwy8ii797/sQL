8f99b58209ef1286e94e7e8375639d1cb3a7847b
dllwy8ii797 <112999490+dllwy8ii797@users.noreply.github.com>
2024-06-06 15:23:35 +0800

    Update and rename c04_11.sql to c04_Q11.sql

--- chapter 4/c04_Q11.sql
+++ chapter 4/c04_Q11.sql
@@ -2,5 +2,5 @@ INSERT INTO ShohinCatalg(shohin_catalg, sum_sell_price, sum_buying_price)
 SELECT shohin_catalg, SUM(sell_price), SUM(buying_price)
 FROM Shohin 
 GROUP BY shohin_catalg;
--- 確認新增的資料
+
 SELECT * FROM ShohinCatalg;
