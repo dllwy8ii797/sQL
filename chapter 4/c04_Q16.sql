dcfb53b3a562bdd2dceaf4c41b6a9243d71a3f60
dllwy8ii797 <112999490+dllwy8ii797@users.noreply.github.com>
2024-06-06 15:21:19 +0800

    Create c04_Q16.sql

---
+++ chapter 4/c04_Q16.sql
@@ -0,0 +1,10 @@
+START TRANSACTION;
+
+UPDATE Shohin
+SET sell_price = sell_price-1000
+WHERE shohin_name = '襯衫';
+
+UPDATE Shohin
+SET sell_price = sell_price+1000
+WHERE shohin_name = 'T侐';
+COMMIT;
